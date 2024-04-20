c363790576b26d85e609cf2357b1497c6153d9a1
Killar.exe <killar.exe@gmail.com>
2024-04-20 22:38:44 +0200

    Add files

---
+++ src/input/key.cpp
@@ -0,0 +1,13 @@
+#include "key.hpp"
+
+void gs::input::setWindow(sf::RenderWindow *window) {
+  priv::internalWindow = window;
+}
+
+void gs::input::updateInputs() {
+  //TODO: Make it work idk man...
+}
+
+void gs::input::updateEvents(sf::Event &event) {
+  //TODO: Bruh idk how it works I've never did SFML before...
+}
